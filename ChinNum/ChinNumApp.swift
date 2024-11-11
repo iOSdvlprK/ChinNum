2412f81115fe0b9144ca30929d13455cdf45278b
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-11-11 21:37:03 +0900

    Initial Commit

---
+++ ChinNum/ChinNumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChinNumApp.swift
+//  ChinNum
+//
+//  Created by joe on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChinNumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
